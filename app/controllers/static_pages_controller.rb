be0a3e9154a8b587b64e923917e8e2fd62485dc8
{ikari3776} <{nekomasa0303@icloud.com}>
2025-06-16 12:14:34 +0900

    静的ページの集約

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def terms; end
+  def policy; end
+  def rules; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
-  resources :rules, only: %i[index]
   resources :ranks, only: %i[index]
   resources :password_resets, only: %i[new create edit update]
-  resources :policy, only: %i[index]
-  resources :terms, only: %i[index]
+  resources :static_pages, path: '/' do
+    collection do
+      get :terms
+      get :policy
+      get :rules
+    end
+  end
   resources :badges, only: %i[index]
   resources :games, only: %i[index] do
     collection do
